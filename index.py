b51514c4f89a39810db724a2ced05f1509370bc7
Collin Norwood <cnorwood7641@gmail.com>
2019-07-06 16:27:56 -0500

    untested

---
+++ index.py
@@ -0,0 +1,22 @@
+#script to just download a random seed of sethblings minecraft randomizer
+#Collin Norwood 2019 (99.99# sethblings work, I wrote just this file)
+
+content = """
+Content Type: text/html\n\n
+
+<html>
+<head>
+<title>title</title>
+</head>
+<body>
+<h1>header</h1>
+<p>paragraph</p>
+</body>
+</html>
+
+"""
+print(content)
+
+
+def seed():
+    print ('pyhton')
