134e0cdaf20f4ff45d81f6dc73467c61d176807b
Collin Norwood <cnorwood7641@gmail.com>
2019-07-06 17:20:16 -0500

    done

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import web
 
 urls = (
-  '/', 'index'
+  '/', 'index',
+  '/download', 'Download'
 )
 
 content = """
 <HTML>
-<Head>
-<Titlte>test</Title>
-</Head>
-<Body>
-<h1>test</h1>
-<br>
-<p>test</p>
-</Body>
+ <Head>
+  <Title>Random loot</Title>
+ </Head>
+ <Body>
+  <h1>Random loot datapack</h1>
+  <br>
+  <p>
+  Install this datapack
+  <br><br>
+  <a href="download">Download - random_loot.zip</a>
+  </p>
+ </Body>
 </HTML>
 """
 
@@ -21,8 +26,16 @@ content = """
 class index:
     def GET(self):
 	import randomize
-	print "run"
+	print 'run'
         return content
+
+class Download:
+	def GET(self):
+		path = 'random_loot.zip'
+		web.header('Content-Disposition', 'attachment; filename="random_loot.zip"')
+           	web.header('Content-type','application/zip')
+           	web.header('Content-transfer-encoding','binary') 
+		return open(path, 'rb').read()
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
